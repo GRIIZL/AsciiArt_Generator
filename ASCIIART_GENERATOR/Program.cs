d9216649fc82969363bfc66d40d409fa6d67b6c6
GRIIZL <72096275+GRIIZL@users.noreply.github.com>
2023-03-09 18:15:08 +0300

    Add project files.

---
+++ ASCIIART_GENERATOR/Program.cs
@@ -0,0 +1,29 @@
+﻿using SkiaSharp;
+using System;
+using System.Drawing;
+
+
+namespace ASCIIART_GENERATOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ascii = new string[] { "#", "@", "%", "=", "+", "*", ":", "-", ".", " " };
+           SKBitmap img = SKBitmap.Decode("breaking-bad-heisenberg-pin-128x128.png");
+            for (int y=0; y<img.Height; y++)
+            {
+                for (int x = 0; x < img.Width; x++)
+                {
+                    SKColor color = img.GetPixel(x, y); 
+                    int grayColor = (color.Red + color.Green + color.Blue) / 3;
+                    int index = grayColor * 9 / 255;
+                    Console.Write(ascii[index]);
+                }
+
+                Console.Write(Environment.NewLine);
+            }
+            Console.ReadKey();
+        }
+    }
+}
